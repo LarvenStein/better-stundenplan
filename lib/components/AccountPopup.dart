f1dc12ebcce47b85ae0680d1d7ee3d7b84a25c7e
Eike Wilkens <eike.wilkens@wilasd.de>
2025-03-13 08:48:49 +0100

    Added about tab

--- lib/components/AccountPopup.dart
+++ lib/components/AccountPopup.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../providers/authenticationProvider.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 void showAccountPopup(BuildContext context) {
   var simpleDialog = SimpleDialog(
     title: Text('Better Stundenplan'),
     children: [
       Divider(),
+      GestureDetector(
+      onTap: () async {
+        PackageInfo packageInfo = await PackageInfo.fromPlatform();
+        if (context.mounted) {
+           showAboutDialog(
+          context: context,
+          applicationName: packageInfo.appName,
+          applicationVersion: packageInfo.version,
+          applicationIcon: Image.asset("assets/icon.png"),
+        );
+      }
+    },
+  child: ListTile(
+    leading: Icon(Icons.info_outlined),
+    title: Text(
+      "Über",
+      style: TextStyle(
+        fontSize: 18,
+      ),
+    ),
+  ),
+  ),
       GestureDetector(
         onTap: () {
           deleteAuthentication();
