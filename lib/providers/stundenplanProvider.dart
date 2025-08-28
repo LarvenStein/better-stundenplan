7a1c73d27e6939b01e049b434968fbf4f1b6da84
Eike Wilkens <eike.wilkens@wilasd.de>
2025-08-28 20:01:55 +0200

    Always getting data from `page2` and closing #3!

--- lib/providers/stundenplanProvider.dart
+++ lib/providers/stundenplanProvider.dart
@@ -1,15 +1,49 @@
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:html/parser.dart' as htmlParser;
 import 'package:html/dom.dart' as dom;
 
+DateTime getMonday(String date) {
+
+  DateTime parsedDate = DateFormat("dd.MM.yyyy").parse(date);
+  int daysFromMonday = parsedDate.weekday - 1;
+  return parsedDate.subtract(Duration(days: daysFromMonday));
+}
+
 Future<List<List<List<Map<String, String>>>>> getWeeklyStundenplan(String date, {String page = 'page-5'}) async {
+  if(page != "page-5") {
+    return getDailyStundenplan(date);
+  }
+
+  DateTime weekDay = getMonday(date);
+
+  List<List<List<Map<String, String>>>> weeklyTimetable = [];
+
+  for (int i = 0; i < 7; i++) {
+    DateTime currentDay = weekDay.add(Duration(days: i));
+
+    String formattedDate = DateFormat("dd.MM.yyyy").format(currentDay);
+
+    var timetableOfDay = await getDailyStundenplan(formattedDate);
+
+    if (i == 0) {
+      weeklyTimetable.add(timetableOfDay[0]);
+    }
+
+    weeklyTimetable.add(timetableOfDay[1]);
+  }
+
+  return weeklyTimetable;
+}
+
+Future<List<List<List<Map<String, String>>>>> getDailyStundenplan(String date) async {
   var prefs = await SharedPreferences.getInstance();
   String sessionId = prefs.getString("sessionId") ?? "";
 
   try {
     final response = await http.get(
-      Uri.parse("https://virtueller-stundenplan.org/$page/index.php?KlaBuDatum=$date"),
+      Uri.parse("https://virtueller-stundenplan.org/page2/index.php?KlaBuDatum=$date"),
       headers: {
         'Cookie': 'PHPSESSID=$sessionId',
       },
